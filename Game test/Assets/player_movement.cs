11d563ccadb65ee3c4e3a3db14ddc50cb38928a3
Flap jack <stingbear@sbcglobal.net>
2020-10-06 17:45:46 -0700

    initial movements

---
+++ Game test/Assets/player_movement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_movement : MonoBehaviour
+{
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            anim.SetTrigger("Walk");
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            anim.SetTrigger("Idle");
+        }
+    }
+}
