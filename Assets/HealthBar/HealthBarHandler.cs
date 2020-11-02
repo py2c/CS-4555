55d8617f4d1c4dbc0d5d786fb3f7c6a3f1906c02
Khang Le <69871123+angleKH@users.noreply.github.com>
2020-11-02 12:38:46 -0800

    Added HP and damage

---
+++ Assets/HealthBar/HealthBarHandler.cs
@@ -0,0 +1,53 @@
+﻿//https://gyanendushekhar.com/2019/11/17/create-health-bar-unity-3d/
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarHandler : MonoBehaviour
+{
+    private static Image HealthBarImage;
+ 
+    /// <summary>
+    /// Sets the health bar value
+    /// </summary>
+    /// <param name="value">should be between 0 to 1</param>
+    public static void SetHealthBarValue(float value)
+    {
+        HealthBarImage.fillAmount = value;
+        if(HealthBarImage.fillAmount < 0.2f)
+        {
+            SetHealthBarColor(Color.red);
+        }
+        else if(HealthBarImage.fillAmount < 0.4f)
+        {
+            SetHealthBarColor(Color.yellow);
+        }
+        else
+        {
+            SetHealthBarColor(Color.green);
+        }
+    }
+ 
+    public static float GetHealthBarValue()
+    {
+        return HealthBarImage.fillAmount;
+    }
+ 
+    /// <summary>
+    /// Sets the health bar color
+    /// </summary>
+    /// <param name="healthColor">Color </param>
+    public static void SetHealthBarColor(Color healthColor)
+    {
+        HealthBarImage.color = healthColor;
+    }
+ 
+    /// <summary>
+    /// Initialize the variable
+    /// </summary>
+    private void Awake()
+    {
+        HealthBarImage = GetComponent<Image>();
+    }
+}
+
---
+++ Assets/KnightStats.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnightStats : MonoBehaviour
+{
+    [SerializeField]
+    private static int current_hp = 500;
+    [SerializeField]
+    private static int max_hp = 500;
+    
+    void Start()
+    {
+        HealthBarHandler.SetHealthBarValue(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //dealDamage(1);
+    }
+    
+    public static void dealDamage(int x)
+    {
+        current_hp -= x;
+        HealthBarHandler.SetHealthBarValue(((float)current_hp)/max_hp);
+        if (current_hp < 0) {
+            //Game over
+        }
+    }
+    
+}
